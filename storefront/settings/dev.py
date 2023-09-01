8b8eb5e1642f79ca3f2360eb3e8f3165ebccc38b
Author <nab@nabeel.com>
2023-09-02 03:41:10 +0500

    change dev file database commented out

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -5,7 +5,7 @@ from .common import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oat7@d1zr*s!0frtf%5pdxiy1xthn$t&u%^_x%m14elf#n8%8e'
+#SECRET_KEY = 'oat7@d1zr*s!0frtf%5pdxiy1xthn$t&u%^_x%m14elf#n8%8e'
 
 
 # DATABASES = {
