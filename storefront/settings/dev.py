1919e2a9f91d69709406529df6ac01715ecd9185
Author <nab@nabeel.com>
2023-09-02 02:43:24 +0500

    change dev file database commented out

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -8,12 +8,12 @@ DEBUG = True
 SECRET_KEY = 'oat7@d1zr*s!0frtf%5pdxiy1xthn$t&u%^_x%m14elf#n8%8e'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront3',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'P@ssword'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'storefront3',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'P@ssword'
+#     }
+# }
